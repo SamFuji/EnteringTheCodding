e6090e654776c1c1a4325aae738e19190fe02d5f
SamFuji <samfuji174@mail.ru>
2022-08-08 00:06:05 +0500

    Task One HW

---
+++ SeminarC_Sharp_HW1/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+// Программа, получающая на вводе два числа, а на выходе показывающая максимаольное значение
+
+// Console.Write("Enter the number1:"); //Консоль пишет:
+// int number1 = Convert.ToInt32( Console.ReadLine()); //Консоль считывает введённое число
+// Console.Write("Enter the number2:"); //после этого консоль запрашивает второе число
+// int number2 = Convert.ToInt32( Console.ReadLine()); //Вводим второе
+// int max;
+// if(number1 >= number2)
+// {
+//    Console.WriteLine("The max number is:" + max);
+// }
+// else       
+//    Console.WriteLine("The max number is:" + number2);
+
+//Второй вариант решения:
+
+Console.Write("Enter the number1:"); //Консоль пишет:
+int number1 = Convert.ToInt32( Console.ReadLine()); //Консоль считывает введённое число
+Console.Write("Enter the number2:"); //после этого консоль запрашивает второе число
+int number2 = Convert.ToInt32( Console.ReadLine()); //Вводим второе
+int max;
+if(number1 >= number2)
+    max=number1;
+else max=number2;       
+    Console.WriteLine("The max number is:" + max);
