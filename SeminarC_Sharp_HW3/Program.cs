4abb2a11f913ccd89f4667b391778cde078b860b
SamFuji <samfuji174@mail.ru>
2022-08-08 23:47:28 +0500

    Homework task 6

---
+++ SeminarC_Sharp_HW3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.Write("Enter the number:"); 
+int number = Convert.ToInt32( Console.ReadLine()); 
+if (number %2!=0)
+{
+    Console.Write("The number " + number + " is: ODD");
+}
+else        
+    Console.WriteLine("The number " + number + " is: EVEN");
