463be7461715428775f7a6c9928c3835c9dc7808
SamFuji <samfuji174@mail.ru>
2022-08-08 23:25:34 +0500

    Homework task 4

--- SeminarC_Sharp_HW2/Program.cs
+++ SeminarC_Sharp_HW2/Program.cs
@@ -1,21 +1,5 @@
 ﻿Console.Clear();
 
-// Console.Write("Enter the number1:"); 
-// int number1 = Convert.ToInt32( Console.ReadLine()); 
-// Console.Write("Enter the number2:"); 
-// int number2 = Convert.ToInt32( Console.ReadLine()); 
-// Console.Write("Enter the number3:"); 
-// int number3 = Convert.ToInt32( Console.ReadLine()); 
-// int max;
-// if(number1 >= number2)
-//     max = number1
-// {
-//    Console.WriteLine("The max number is:" + max);
-// }
-// else       
-//    Console.WriteLine("The max number is:" + number2);
-
-
 // нахождение максимального числа из трёх чисел
 
 int number1 = new Random().Next(1,1000001);
