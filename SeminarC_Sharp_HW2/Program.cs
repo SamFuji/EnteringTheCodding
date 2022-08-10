accd7d94b388bed51d4f67bcacade1d784c953b2
SamFuji <samfuji174@mail.ru>
2022-08-10 22:30:33 +0500

    Homework task 6

--- SeminarC_Sharp_HW2/Program.cs
+++ SeminarC_Sharp_HW2/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Clear();
 
-// нахождение максимального числа из трёх чисел
+нахождение максимального числа из трёх чисел
 
 int number1 = new Random().Next(1,1000001);
 int number2 = new Random().Next(1,1000001); 
@@ -14,5 +14,8 @@ if (number2 >= max) max = number2;
 if (number3 >= max) max = number3;
 
 Console.WriteLine("Maximum is: " + max);
+
+
+
 
 
