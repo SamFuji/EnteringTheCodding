2d9fbae8f53966f873c136be468005594e8c6b80
SamFuji <samfuji174@mail.ru>
2022-08-10 23:13:55 +0500

    Homework task 8

---
+++ SeminarC_Sharp_HW4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+Console.Clear();
+
+Console.Write("Enter the number:");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+Console.WriteLine(N);
+
+if (N<2)
+{
+    Console.Write("false, the numbber must to be large");
+
+}
+else
+
+    while (i <= N)
+{
+    Console.Write(i);
+    i+=2;
+}
+
